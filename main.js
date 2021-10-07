b338a78bf70bc521f4e812589ea26d75ad8dc875
Jakub Aleksander <jakubaleksander@iMac.local>
2021-10-07 12:18:17 +0000

    js fixes

--- main.js
+++ main.js
@@ -12,13 +12,18 @@ var favButton = classes("button"),
 remButton = classes("button2"),
 starOutline = classes("starNotClicked"),
 starFill = classes("starClicked"),
-goYourFavoriteHerosButton = classes("buttonHead");
+goYourFavoriteHerosButton = classes("buttonHead"),
+goBackButton = classes("buttonHead2");
 
 
 
 favButton[0].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.setItem("Spider-man", "favorite")
+    localStorage.setItem("Spider-man", 
+                        `<h2>SPIDER-MAN
+                        <img class="starClicked" src="https://github.com/JakubM1999/Module3-Javascript-APIs/blob/main/img/star2.png?raw=true" alt="star">
+                        </h2>
+                        <img class="hero1" src="img/hero1.png" alt="Spider-Man">`)
     starOutline[0].style.opacity = 0;
     starFill[0].style.opacity = 1;
     remButton[0].style.display = "initial";
@@ -27,7 +32,7 @@ favButton[0].addEventListener("click", (e)=>{
 
   remButton[0].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.removeItem("Spider-man", "favorite")
+    localStorage.removeItem("Spider-man")
     starOutline[0].style.opacity = 1;
     starFill[0].style.opacity = 0;
     remButton[0].style.display = "none";
@@ -36,7 +41,10 @@ favButton[0].addEventListener("click", (e)=>{
 
   favButton[1].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.setItem("Goku", "favorite")
+    localStorage.setItem("Goku", `<h2>GOKU
+                                <img class="starClicked" src="https://github.com/JakubM1999/Module3-Javascript-APIs/blob/main/img/star2.png?raw=true" alt="star">
+                                </h2>
+                                <img class="hero2" src="img/hero2.png" alt="Goku">`)
     starOutline[1].style.opacity = 0;
     starFill[1].style.opacity = 1;
     remButton[1].style.display = "initial";
@@ -45,7 +53,7 @@ favButton[0].addEventListener("click", (e)=>{
 
   remButton[1].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.removeItem("Goku", "favorite")
+    localStorage.removeItem("Goku")
     starOutline[1].style.opacity = 1;
     starFill[1].style.opacity = 0;
     remButton[1].style.display = "none";
@@ -54,7 +62,10 @@ favButton[0].addEventListener("click", (e)=>{
 
   favButton[2].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.setItem("Sonic", "favorite")
+    localStorage.setItem("Sonic", `<h2>SONIC
+                                    <img class="starClicked" src="https://github.com/JakubM1999/Module3-Javascript-APIs/blob/main/img/star2.png?raw=true" alt="star">
+                                    </h2>
+                                    <img class="hero3" src="img/hero3.png" alt="Sonic">`)
     starOutline[2].style.opacity = 0;
     starFill[2].style.opacity = 1;
     remButton[2].style.display = "initial";
@@ -63,7 +74,7 @@ favButton[0].addEventListener("click", (e)=>{
 
   remButton[2].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.removeItem("Sonic", "favorite")
+    localStorage.removeItem("Sonic")
     starOutline[2].style.opacity = 1;
     starFill[2].style.opacity = 0;
     remButton[2].style.display = "none";
@@ -72,7 +83,10 @@ favButton[0].addEventListener("click", (e)=>{
 
   favButton[3].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.setItem("Rayman", "favorite")
+    localStorage.setItem("Rayman", `<h2>RAYMAN
+                        <img class="starClicked" src="https://github.com/JakubM1999/Module3-Javascript-APIs/blob/main/img/star2.png?raw=true" alt="star">
+                        </h2>
+                        <img class="hero4" src="img/hero4.png" alt="Rayman">`)
     starOutline[3].style.opacity = 0;
     starFill[3].style.opacity = 1;
     remButton[3].style.display = "initial";
@@ -81,7 +95,7 @@ favButton[0].addEventListener("click", (e)=>{
 
   remButton[3].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.removeItem("Rayman", "favorite")
+    localStorage.removeItem("Rayman")
     starOutline[3].style.opacity = 1;
     starFill[3].style.opacity = 0;
     remButton[3].style.display = "none";
@@ -90,7 +104,10 @@ favButton[0].addEventListener("click", (e)=>{
 
   favButton[4].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.setItem("Crash", "favorite")
+    localStorage.setItem("Crash", `<h2>CRASH
+                        <img class="starClicked" src="https://github.com/JakubM1999/Module3-Javascript-APIs/blob/main/img/star2.png?raw=true" alt="star">
+                        </h2>
+                        <img class="hero5" src="img/hero5.png" alt="Crash">`)
     starOutline[4].style.opacity = 0;
     starFill[4].style.opacity = 1;
     remButton[4].style.display = "initial";
@@ -99,7 +116,7 @@ favButton[0].addEventListener("click", (e)=>{
 
   remButton[4].addEventListener("click", (e)=>{
     e.preventDefault();
-    localStorage.removeItem("Crash", "favorite")
+    localStorage.removeItem("Crash")
     starOutline[4].style.opacity = 1;
     starFill[4].style.opacity = 0;
     remButton[4].style.display = "none";
@@ -108,16 +125,33 @@ favButton[0].addEventListener("click", (e)=>{
 
 
 var favoriteHeroPage = () => {
+    h1.innerHTML= "Your Favorite Heros"
     card1.innerHTML= "";
     card2.innerHTML= "";
     card3.innerHTML= "";
     card4.innerHTML= "";
     card5.innerHTML= "";
+
+    var x = localStorage.getItem("Spider-man")
+    card1.innerHTML = x;
+
+    var x = localStorage.getItem("Goku")
+    card2.innerHTML = x;
+
+    var x = localStorage.getItem("Sonic")
+    card3.innerHTML = x;
+
+    var x = localStorage.getItem("Rayman")
+    card4.innerHTML = x;
+
+    var x = localStorage.getItem("Crash")
+    card5.innerHTML = x;
 }
 
-var getHero = () => {
-    var x = localStorage.getItem("spiderman", "favorite");
-    document.getElementById("card1").innerHTML = x;
-}
-goYourFavoriteHerosButton[0].addEventListener("click", getHero);
 goYourFavoriteHerosButton[0].addEventListener("click", favoriteHeroPage);
+goYourFavoriteHerosButton[0].addEventListener("click", (e) => {
+    e.preventDefault();
+    goYourFavoriteHerosButton[0].style.display = "none";
+    
+
+})
